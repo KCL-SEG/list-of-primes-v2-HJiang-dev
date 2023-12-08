7ea6ac8765856313423018dcee0527fd6752fe17
HJiang-dev <127912159+HJiang-dev@users.noreply.github.com>
2023-12-08 15:55:56 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,6 +1,26 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
 def primes(number_of_primes):
     list = []
+    n = 2
+    while len(list) < number_of_primes:
+        if is_prime(n):
+            list.append(n)
+        n += 1
+
     return list
